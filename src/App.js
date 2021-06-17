86374795fa7744271dd6f8859763c7727c1fd1ce
nstwebd <nst.webd@gmail.com>
2021-06-17 21:49:43 +0530

    basic component structure done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <h1>Beach Resort App</h1>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/" exact component={Rooms} />
+        <Route path="/rooms/:slug" exact component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SingleRoom extends Component {
+  render() {
+    return (
+      <div>
+        <p>Helloo !! Single Room Page</p>
+      </div>
+    );
+  }
+}
+
+export default SingleRoom;
